3433dcc3a42e706ba89c8ca6c7fae76147f3fc20
Ivan Linkevich <79784960+Ivan6813@users.noreply.github.com>
2025-05-13 20:49:43 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,14 +12,8 @@ export default defineConfig(({ command }) => ({
     },
     resolve: {
         alias: {
-            '@public': path.resolve(__dirname, 'public'),
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@constants': path.resolve(__dirname, 'src/constants'),
-            '@hooks': path.resolve(__dirname, 'src/hooks'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@redux': path.resolve(__dirname, 'src/redux'),
-            '@types': path.resolve(__dirname, 'src/types'),
-            '@utils': path.resolve(__dirname, 'src/utils'),
+            '~': resolve(__dirname, 'src'),
+            '@public': resolve(__dirname, 'public'),
         },
     },
 }));
